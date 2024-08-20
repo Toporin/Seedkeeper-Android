8fd41e0fb54dd3ef706500ae0a7509a76881e489
Petar Sebic <petarsebic@invmac036l.local>
2024-08-20 16:46:20 +0200

    feature: backup process now uses encryption

--- app/src/main/java/org/satochip/seedkeeper/ui/components/generate/SecretTextField.kt
+++ app/src/main/java/org/satochip/seedkeeper/ui/components/generate/SecretTextField.kt
@@ -46,6 +46,7 @@ fun SecretTextField(
     curValue: MutableState<String>,
     containerColor: Color = SatoDividerPurple.copy(alpha = 0.2f),
     isQRCodeEnabled: Boolean = true,
+    maxLines: Int = 3,
     visualTransformation: VisualTransformation = PasswordVisualTransformation(),
     copyToClipboard: () -> Unit
 ) {
@@ -69,7 +70,7 @@ fun SecretTextField(
         Row(
             modifier = Modifier
             .padding(8.dp)
-            .width(96.dp)
+            .width(if (isQRCodeEnabled) 96.dp else 64.dp)
             .align(Alignment.TopEnd)
         ) {
             if (isQRCodeEnabled) {
@@ -164,6 +165,7 @@ fun SecretTextField(
                     disabledTextColor = Color.Black,
                 ),
                 minLines = 1,
+                maxLines = maxLines
             )
         }
     }
