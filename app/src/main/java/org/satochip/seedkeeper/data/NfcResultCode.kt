6dba4a2cbcb1868cedb51110820714cf1465a2a6
Petar Sebic <petarsebic@invmac036l.local>
2024-08-23 15:54:09 +0200

    hotfix: changed nfc text when card scanning for backup

--- app/src/main/java/org/satochip/seedkeeper/data/NfcResultCode.kt
+++ app/src/main/java/org/satochip/seedkeeper/data/NfcResultCode.kt
@@ -12,7 +12,7 @@ enum class  NfcResultCode(val resTitle : Int, val resMsg : Int, val resImage : I
     SECRET_FETCHED_SUCCESSFULLY(R.string.nfcTitleSuccess, R.string.secretFetchedSuccessfully, R.drawable.icon_check_gif, null),
     CARD_LABEL_CHANGED_SUCCESSFULLY(R.string.nfcTitleSuccess, R.string.cardLabelChangedSuccessfully, R.drawable.icon_check_gif, null),
     CARD_SETUP_SUCCESSFUL(R.string.nfcTitleSuccess, R.string.cardSetupSuccessful, R.drawable.icon_check_gif, null),
-    CARD_SUCCESSFULLY_SCANNED(R.string.nfcTitleSuccess, R.string.cardLabelChangedSuccessfully, R.drawable.icon_check_gif, null),
+    CARD_SUCCESSFULLY_SCANNED(R.string.nfcTitleSuccess, R.string.cardSuccessfullyScanned, R.drawable.icon_check_gif, null),
     CARD_SUCCESSFULLY_BACKED_UP(R.string.nfcTitleSuccess, R.string.cardSuccessfullyBackedUp, R.drawable.icon_check_gif, null),
     UNKNOWN_ERROR(R.string.nfcTitleWarning, R.string.nfcErrorOccured, R.drawable.error_24px, null),
     NONE(R.string.scanning, R.string.nfcResultCodeNone, R.drawable.error_24px, null),
