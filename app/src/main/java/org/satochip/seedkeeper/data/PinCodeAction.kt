6825370eeec62045e24fef58d8246770b228b1fc
Toporin <satochip.wallet@gmail.com>
2024-10-22 20:26:15 +0100

    Remove unused code EditPinButtons, PinCodeStatus

--- app/src/main/java/org/satochip/seedkeeper/data/PinCodeAction.kt
+++ app/src/main/java/org/satochip/seedkeeper/data/PinCodeAction.kt
@@ -1,12 +1,5 @@
 package org.satochip.seedkeeper.data
 
-enum class PinCodeStatus {
-    CURRENT_PIN_CODE,
-    INPUT_NEW_PIN_CODE,
-    CONFIRM_PIN_CODE,
-    WRONG_PIN_CODE
-}
-
 enum class PinCodeAction {
     ENTER_PIN_CODE, // ENTER_PIN_CODE -> end
     SETUP_PIN_CODE, // SETUP_PIN_CODE -> CONFIRM_PIN_CODE -> end
